9381249fe0119bfaed3c7dcc4bf71edd6d336fec
Lee Pike <leepike@gmail.com>
2012-12-04 22:33:50 -0800

    paper mods.

--- paper/Paper.hs
+++ paper/Paper.hs
@@ -2,6 +2,9 @@
 
 module Paper where
 
+cast :: SubTypes a => a -> Maybe b
+cast _ = undefined
+
 class Arbitrary a where
   -- arbitrary :: a
 
@@ -19,28 +22,35 @@ class Arbitrary a => SubTypes a where
   index   :: a -> Int -> Maybe SubVal
   replace :: a -> Int -> SubVal -> a
 
-reduce :: SubTypes a => a -> (a -> Property) -> IO a
+reduce :: SubTypes a 
+       => a -> (a -> Property) -> IO a
 reduce cex prop = reduce' 1
   where
-  reduce' idx = 
+  reduce' idx =
     case index cex idx of 
       Nothing -> return cex
       Just v  -> do
         vs <- newVals v
         case test cex idx vs prop of
-          Nothing   -> reduce' (idx + 1)
+          Nothing   -> reduce' (idx+1)
           Just cex' -> reduce cex' prop
 
-  newVals (SubVal a) = sequence (replicate maxTries s)
-    where s = sizedArbitrary a >>= return . SubVal
+newVals :: SubVal -> IO [SubVal]
+newVals (SubVal a) = 
+  sequence (replicate maxTries s)
+  where s =     sizedArbitrary a 
+            >>= return . SubVal
 
 test :: SubTypes a 
-     => a -> Int -> [SubVal] -> (a -> Property) -> Maybe a
+     => a -> Int -> [SubVal] 
+     -> (a -> Property) -> Maybe a
 test cex idx vs prop = go vs
   where
-  go []     = Nothing
-  go (v:vs') = let cex' = replace cex idx v in
-               if pass prop cex' then go vs' else Just cex'
+  go []      = Nothing
+  go (v:vs') = 
+    let cex' = replace cex idx v in
+    if pass prop cex' then go vs' 
+      else Just cex'
 
 sizedArbitrary :: forall a. SubTypes a => a -> IO a
 sizedArbitrary _ = return (undefined :: a)
@@ -52,7 +62,31 @@ maxTries = 100
 pass :: (a -> Property) -> a -> Bool
 pass _ _ = True
 
+reduce0 :: forall a . SubTypes a 
+        => a -> (a -> Property) -> IO a
+reduce0 cex prop = reduce' 1
+  where
+  reduce' idx = 
+    case index cex idx of 
+      Nothing -> return cex
+      Just v  -> 
+        case testHole v of
+          Just v' -> reduce0 v' prop
+          Nothing -> do
+            vs <- newVals v
+            case test cex idx vs prop of
+              Nothing   -> 
+                reduce' (idx+1)
+              Just cex' -> 
+                reduce0 cex' prop
 
+  testHole (SubVal a) = 
+    case cast a :: Maybe a of
+      Nothing -> Nothing
+      Just a' -> 
+        if pass prop a' then Nothing
+          else Just a'
+    
 --------------------------------------------------------------------------------
 
 data Tree = L | B Tree Tree

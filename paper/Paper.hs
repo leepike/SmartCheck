b9448c1ab4c75d00e81744f361a14354d241099d
Lee Pike <leepike@gmail.com>
2012-12-24 10:25:33 -0800

    paper: updates.

--- paper/Paper.hs
+++ paper/Paper.hs
@@ -2,59 +2,64 @@
 
 module Paper where
 
+import Prelude hiding (fail)
+import Data.Maybe (catMaybes)
+
 cast :: SubTypes a => a -> Maybe b
 cast _ = undefined
 
 class Arbitrary a where
-  -- arbitrary :: a
+--  arbitrary :: IO a
 
---type Property a = a -> Bool
+sizedArbitrary :: forall a. SubTypes a => a -> IO a
+sizedArbitrary _ = return (undefined :: a)
+
+unsizedArbitrary :: forall a. SubTypes a => a -> IO a
+unsizedArbitrary _ = return (undefined :: a)
 
 data Property
 
 -- sized :: [a] -> Int -> [a]
 -- sized = undefined
 
-data SubVal = forall a. (Arbitrary a, SubTypes a) => SubVal a
+data SubVal = forall a. SubTypes a => SubVal a
+
+type Idx = Int
 
 class Arbitrary a => SubTypes a where
-  size    :: a -> Int
-  index   :: a -> Int -> Maybe SubVal
-  replace :: a -> Int -> SubVal -> a
+  size    :: a -> Idx
+  index   :: a -> Idx -> Maybe SubVal
+  replace :: a -> Idx -> SubVal -> a
 
-reduce :: SubTypes a 
+reduce :: SubTypes a
        => a -> (a -> Property) -> IO a
 reduce cex prop = reduce' 1
   where
   reduce' idx =
-    case index cex idx of 
+    case index cex idx of
       Nothing -> return cex
       Just v  -> do
-        vs <- newVals v
+        vs <-newVals v
         case test cex idx vs prop of
           Nothing   -> reduce' (idx+1)
           Just cex' -> reduce cex' prop
 
 newVals :: SubVal -> IO [SubVal]
-newVals (SubVal a) = 
+newVals (SubVal a) =
   sequence (replicate maxTries s)
-  where s =     sizedArbitrary a 
-            >>= return . SubVal
+  where
+  s = sizedArbitrary a >>= return . SubVal
 
-test :: SubTypes a 
-     => a -> Int -> [SubVal] 
+test :: SubTypes a
+     => a -> Idx -> [SubVal]
      -> (a -> Property) -> Maybe a
 test cex idx vs prop = go vs
   where
   go []      = Nothing
-  go (v:vs') = 
+  go (v:vs') =
     let cex' = replace cex idx v in
-    if pass prop cex' then go vs' 
+    if pass prop cex' then go vs'
       else Just cex'
-
-sizedArbitrary :: forall a. SubTypes a => a -> IO a
-sizedArbitrary _ = return (undefined :: a)
---sizedArbitrary a n = SubVal (sized arbitrary n)
 
 maxTries :: Int
 maxTries = 100
@@ -62,31 +67,67 @@ maxTries = 100
 pass :: (a -> Property) -> a -> Bool
 pass _ _ = True
 
-reduce0 :: forall a . SubTypes a 
+-- Failed, passed, or failed precondition.
+fail :: (a -> Property) -> a -> Maybe Bool
+fail _ _ = Just True
+
+reduce0 :: forall a . SubTypes a
         => a -> (a -> Property) -> IO a
 reduce0 cex prop = reduce' 1
   where
-  reduce' idx = 
-    case index cex idx of 
+  reduce' idx =
+    case index cex idx of
       Nothing -> return cex
-      Just v  -> 
-        case testHole v of
-          Just v' -> reduce0 v' prop
-          Nothing -> do
-            vs <- newVals v
-            case test cex idx vs prop of
-              Nothing   -> 
-                reduce' (idx+1)
-              Just cex' -> 
-                reduce0 cex' prop
+      Just v  -> case testHole v of
+                   Just v' -> reduce0 v' prop
+                   Nothing -> test' v idx
 
-  testHole (SubVal a) = 
+  test' v idx = do
+    vs <- newVals v
+    case test cex idx vs prop of
+      Nothing   -> reduce' (idx+1)
+      Just cex' -> reduce0 cex' prop
+
+  testHole (SubVal a) =
     case cast a :: Maybe a of
       Nothing -> Nothing
-      Just a' -> 
+      Just a' ->
         if pass prop a' then Nothing
           else Just a'
-    
+
+--------------------------------------------------------------------------------
+
+extrapolate :: SubTypes a
+       => a -> (a -> Property) -> IO [Idx]
+extrapolate cex prop = extrapolate' 1 []
+  where
+  extrapolate' idx idxs =
+    case index cex idx of
+      Nothing -> return idxs
+      Just v  -> do
+        vs <- extrapVals v
+        if allFail cex idx vs prop
+          then extrapolate' (idx+1) (idx:idxs)
+          else extrapolate' (idx+1) idxs
+
+extrapVals :: SubVal -> IO [SubVal]
+extrapVals (SubVal a) =
+  sequence (replicate maxExtrap s)
+  where
+  s = unsizedArbitrary a >>= return . SubVal
+
+allFail :: SubTypes a
+     => a -> Idx -> [SubVal]
+     -> (a -> Property) -> Bool
+allFail cex idx vs prop =
+  (not (null res)) && and res
+  where
+  res  = catMaybes (map (fail prop) vals)
+  vals = map (replace cex idx) vs
+
+maxExtrap :: Int
+maxExtrap = 100
+
 --------------------------------------------------------------------------------
 
 data Tree = L | B Tree Tree
@@ -96,7 +137,7 @@ data Tree = L | B Tree Tree
 --   size (B t0 t1) = 1 + size t0 + size 1
 
 --   -- index t 0 = (Just . SubVal) t
---   -- index (B t0 t1) n = if size t0 >= n then 
+--   -- index (B t0 t1) n = if size t0 >= n then
 
 -- tree = B (B L
 --              (B L L))
@@ -108,7 +149,7 @@ data Tree = L | B Tree Tree
 
 -- index tree 12 = Nothing
 
--- replace tree 2 (SubVal L) = 
---   B (B L 
+-- replace tree 2 (SubVal L) =
+--   B (B L
 --         (B L L))
 --      L

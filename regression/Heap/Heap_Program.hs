0f7528852b4ba363a808c287f1de2288ee0ea0ad
Lee Pike <leepike@gmail.com>
2014-03-16 21:49:01 -0700

    regression: add another file.

---
+++ regression/Heap/Heap_Program.hs
@@ -0,0 +1,224 @@
+{-# LANGUAGE CPP #-}
+
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell, DeriveDataTypeable, StandaloneDeriving #-}
+{-# LANGUAGE DeriveGeneric #-}
+
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+-- Copied from QuickCheck2's examples.
+
+module Main where
+
+--------------------------------------------------------------------------
+-- imports
+
+import Test
+import Test.SmartCheck
+import System.Environment
+
+import Test.QuickCheck
+import Test.QuickCheck.Poly
+
+import Data.List
+  ( sort
+  , (\\)
+  )
+import Data.Typeable
+
+import GHC.Generics
+
+import qualified Test.SmartCheck as SC
+
+--------------------------------------------------------------------------
+-- SmartCheck Testing.  Comment out shrink instance if you want to be more
+-- impressed. :)
+--------------------------------------------------------------------------
+
+deriving instance Typeable OrdA
+deriving instance Generic OrdA
+
+heapProgramTest :: IO ()
+heapProgramTest = SC.smartCheck SC.scStdArgs prop_ToSortedList
+
+instance SC.SubTypes OrdA
+instance (SC.SubTypes a, Ord a, Arbitrary a, Generic a)
+         => SC.SubTypes (Heap a)
+instance (SC.SubTypes a, Arbitrary a, Generic a)
+         => SC.SubTypes (HeapP a)
+instance (SC.SubTypes a, Ord a, Arbitrary a, Generic a)
+         => SC.SubTypes (HeapPP a)
+
+instance (Ord a, Arbitrary a) => Arbitrary (Heap a) where
+  arbitrary = do p <- arbitrary :: Gen (HeapP a)
+                 return $ heap p
+
+--------------------------------------------------------------------------
+-- skew heaps
+-- Smallest values on top.
+
+data Heap a
+  = Node a (Heap a) (Heap a)
+  | Nil
+ deriving ( Eq, Ord, Show, Typeable, Generic )
+
+empty :: Heap a
+empty = Nil
+
+isEmpty :: Heap a -> Bool
+isEmpty Nil = True
+isEmpty _   = False
+
+unit :: a -> Heap a
+unit x = Node x empty empty
+
+size :: Heap a -> Int
+size Nil            = 0
+size (Node _ h1 h2) = 1 + size h1 + size h2
+
+insert :: Ord a => a -> Heap a -> Heap a
+insert x h = unit x `merge` h
+
+removeMin :: Ord a => Heap a -> Maybe (a, Heap a)
+removeMin Nil            = Nothing
+removeMin (Node x h1 h2) = Just (x, h1 `merge` h2)
+
+merge :: Ord a => Heap a -> Heap a -> Heap a
+h1  `merge` Nil = h1
+Nil `merge` h2  = h2
+h1@(Node x h11 h12) `merge` h2@(Node y h21 h22)
+  | x <= y    = Node x (h12 `merge` h2) h11
+  | otherwise = Node y (h22 `merge` h1) h21
+
+fromList :: Ord a => [a] -> Heap a
+fromList xs = merging [ unit x | x <- xs ]
+ where
+  merging []  = empty
+  merging [h] = h
+  merging hs  = merging (sweep hs)
+
+  sweep []         = []
+  sweep [h]        = [h]
+  sweep (h1:h2:hs) = (h1 `merge` h2) : sweep hs
+
+toList :: Heap a -> [a]
+toList h = toList' [h]
+ where
+  toList' []                  = []
+  toList' (Nil          : hs) = toList' hs
+  toList' (Node x h1 h2 : hs) = x : toList' (h1:h2:hs)
+
+toSortedList :: Ord a => Heap a -> [a]
+toSortedList Nil            = []
+toSortedList (Node x h1 h2) = x : toList (h1 `merge` h2)
+
+--------------------------------------------------------------------------
+-- heap programs
+
+data HeapP a
+  = Empty
+  | Unit a
+  | Insert a (HeapP a)
+  | SafeRemoveMin (HeapP a)
+  | Merge (HeapP a) (HeapP a)
+  | FromList [a]
+ deriving ( Show, Typeable, Generic )
+
+heap :: Ord a => HeapP a -> Heap a
+heap Empty             = empty
+heap (Unit x)          = unit x
+heap (Insert x p)      = insert x (heap p)
+heap (SafeRemoveMin p) = case removeMin (heap p) of
+                           Nothing    -> empty -- arbitrary choice
+                           Just (_,h) -> h
+heap (Merge p q)       = heap p `merge` heap q
+heap (FromList xs)     = fromList xs
+
+instance Arbitrary a => Arbitrary (HeapP a) where
+  arbitrary = sized arbHeapP
+   where
+    arbHeapP s =
+      frequency
+      [ (1, do return Empty)
+      , (1, do x <- arbitrary
+               return (Unit x))
+      , (s, do x <- arbitrary
+               p <- arbHeapP s1
+               return (Insert x p))
+      , (s, do p <- arbHeapP s1
+               return (SafeRemoveMin p))
+      , (s, do p <- arbHeapP s2
+               q <- arbHeapP s2
+               return (Merge p q))
+      , (1, do xs <- arbitrary
+               return (FromList xs))
+      ]
+     where
+      s1 = s-1
+      s2 = s`div`2
+
+
+  shrink (Unit x)          = [ Unit x' | x' <- shrink x ]
+  shrink (FromList xs)     = [ Unit x | x <- xs ]
+                          ++ [ FromList xs' | xs' <- shrink xs ]
+  shrink (Insert x p)      = [ p ]
+                          ++ [ Insert x p' | p' <- shrink p ]
+                          ++ [ Insert x' p | x' <- shrink x ]
+  shrink (SafeRemoveMin p) = [ p ]
+                          ++ [ SafeRemoveMin p' | p' <- shrink p ]
+  shrink (Merge p q)       = [ p, q ]
+                          ++ [ Merge p' q | p' <- shrink p ]
+                          ++ [ Merge p q' | q' <- shrink q ]
+  shrink _                 = []
+
+data HeapPP a = HeapPP (HeapP a) (Heap a)
+ deriving ( Show, Typeable, Generic )
+
+instance (Ord a, Arbitrary a) => Arbitrary (HeapPP a) where
+  arbitrary =
+    do p <- arbitrary
+       return (HeapPP p (heap p))
+
+  shrink (HeapPP p _) =
+    [ HeapPP p' (heap p') | p' <- shrink p ]
+
+--------------------------------------------------------------------------
+-- properties
+
+(==?) :: Heap OrdA -> [OrdA] -> Bool
+h ==? xs = sort (toList h) == sort xs
+
+prop_ToSortedList :: HeapPP OrdA -> Bool
+prop_ToSortedList (HeapPP _ h) =
+  h ==? xs && xs == sort xs
+ where
+  xs = toSortedList h
+
+sizePP :: HeapPP a -> Int
+sizePP (HeapPP h0 h1) = sizeP h0 + sizeH h1
+
+sizeP :: HeapP a -> Int
+sizeP heap = case heap of
+  Empty           -> 1
+  Unit _          -> 1
+  Insert _ h      -> 1 + sizeP h
+  SafeRemoveMin h -> 1 + sizeP h
+  Merge h0 h1     -> 1 + sizeP h0 + sizeP h1
+  FromList ls     -> 1 + length ls
+
+sizeH :: Heap a -> Int
+sizeH heap = case heap of
+  Node a h0 h1 -> 1 + sizeH h0 + sizeH h1
+  Nil          -> 1
+
+main :: IO ()
+main = do
+  [file', rnds'] <- getArgs
+  let rnds = read rnds' :: Int
+  let file  = read file' :: String
+#ifdef qc
+  test file rnds (runQC' stdArgs prop_ToSortedList) (sizePP :: HeapPP OrdA -> Int)
+#else
+  test file rnds (runSC prop_ToSortedList) sizePP
+#endif
+
